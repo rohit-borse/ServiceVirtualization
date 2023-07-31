2786675a4a399252d472694d96898f283186d2bf
rohit-borse <72081496+rohit-borse@users.noreply.github.com>
2023-07-31 12:22:03 +0100

    Commit 31/07/2023

---
+++ IFS10 SPT/Logical/PerformanceTestingFramework.scm/config.scm
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<editableResource createdTimestamp="1690643218912" createdUser="RBO152"
+    id="24289fbb:18998672e23:-3c6b" type="service_component_resource"
+    updatedTimestamp="1690643218912" updatedUser="RBO152" version="1.1">
+    <loggingConfiguration debug="false" writeStubEventsToDB="false"/>
+</editableResource>
